58191aaa42aecd4cf93006f4cd4c9ae82cc744f4
abhinav3004 <abhiabhinav007@gmail.com>
2019-03-07 20:00:48 -0700

    Tier-2 home page &account detail page

---
+++ src/views/About.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="about">
+    <h1>Tier-2 Home page</h1>
+    <button>Create Account</button>
+    <table>
+      <thead>
+        <tr>
+          <th>Customer Name</th>
+          <th>Account Number</th>
+          <th></th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+       <td>Abhinav</td>
+       <td> 
+          <div id="nav">
+           <router-link to="/accountdetails">1215042985</router-link>
+            </div>
+            <router-view/>
+        </td>
+       <td><button>Modify</button> </td>
+       <td><button>Delete</button></td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+
+</template>
+
+  
---
+++ src/views/AccountDetails.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="about">
+    <h1>Account Details</h1>
+<table style="width:100%">
+  <tr>
+    <th>Name:</th>
+    <td>Bill Gates</td>
+  </tr>
+  <tr>
+    <th>Customer Account Id:</th>
+    <td>555 77 854</td>
+  </tr>
+  <tr>
+    <th>SSN:</th>
+    <td>374 234 2855</td>
+  </tr>
+  <tr>
+    <th>Account Balance:</th>
+    <td>$1500</td>
+  </tr>
+</table>
+
+<table>
+<thead>
+        <tr>
+          <th>Customer Name</th>
+          <th>Request Amount</th>
+          <th></th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+       <td>Abhinav Mishra</td>
+       <td> $1100</td>
+       <td><button type="radio">Accept</button> </td>
+       <td><button type="radio">Decline</button> </td>
+        </tr>
+      </tbody>
+</table>
+  </div>
+</template>
+
+  
