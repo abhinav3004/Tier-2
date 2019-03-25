5916e5e436d2221b3244ca6f8379c02b180fd244
Shaojia Zhao <zhaoshaojia@gmail.com>
2019-03-25 16:51:00 -0700

    Update router.js

--- src/router.js
+++ src/router.js
@@ -34,6 +34,11 @@ export default new Router({
       name: 'manager',
       component: () => import(/* webpackChunkName: "about" */ './views/Manager.vue'),
     },
+    {
+      path: '/Tier2Main',
+      name: 'Tier2Main',
+      component: () => import(/* webpackChunkName: "about" */ './views/Tier2Main.vue'),
+    },
 
   ],
 });
